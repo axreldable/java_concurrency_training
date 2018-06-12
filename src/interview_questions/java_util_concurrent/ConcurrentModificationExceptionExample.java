2537fbad68af11835417f15206e522a1c827a902
Aleksei Starikov <starikovaleksei1992@mail.ru>
2018-06-12 15:34:39 +0300

    add ConcurrentModificationException example

---
+++ src/interview_questions/java_util_concurrent/ConcurrentModificationExceptionExample.java
@@ -0,0 +1,40 @@
+package interview_questions.java_util_concurrent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConcurrentModificationExceptionExample {
+    private final static Long CONST = 1_000_000L;
+    private static List<Long> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        initList();
+        System.out.println("int done");
+
+        Runnable reader = () -> {
+            for (Long i : list) {
+                if (i == 5) {
+                    list.remove(i);
+                }
+            }
+        }; // Забавно ConcurrentModificationException вообще не относится к многопоточности,
+        // а забано то, что 5 элемент все равно удалилися, не сомтря на Exception.
+
+        Runnable writer = () -> list.set(0, 25L);
+
+        Thread threadReader = new Thread(reader);
+        Thread threadWriter = new Thread(writer);
+
+        threadReader.start();
+        threadWriter.start();
+
+        System.out.println("!");
+        list.stream()
+                .limit(10)
+                .forEach(System.out::println);
+    }
+
+    private static void initList() {
+        for (long i = 0; i < CONST; i++) list.add(i);
+    }
+}
